4790475fa140298bd5de8ce3ec37f2c079034a27
Arslanali <arslanali.98@mail.ru>
2020-09-04 18:03:40 +0300

    Create MainModule

---
+++ DIITPeretz/MainModule/Presenter/MainPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MainPresenter.swift
+//  DIITPeretz
+//
+//  Created by arsik on 04.09.2020.
+//  Copyright © 2020 arsik. All rights reserved.
+//
+
+import Foundation
---
+++ DIITPeretz/MainModule/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DIITPeretz
+//
+//  Created by arsik on 04.09.2020.
+//  Copyright © 2020 arsik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+   
+    print("Mesagge")
+  }
+}
+
